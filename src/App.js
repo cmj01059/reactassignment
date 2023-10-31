62fd74e8efa1afe275fc5ac2c6ccace53bfe8b34
cmj01059 <cmj01059@uga.edu>
2023-10-31 15:27:58 -0400

    add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import User from './components/User';
 import AddUser from './components/AddUser';
 import Hdr from './components/Hdr';
+import UsersList from './components/UsersList';
 
 function App() {
+
   return (
     <div>
       <Hdr />
       <AddUser />
-      <User />
+      <UsersList users={usersList} />
     </div>
   );
 }
