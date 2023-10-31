fb1f564ec28b102931eeed56ca2e53672a8f2f0e
cmj01059 <cmj01059@uga.edu>
2023-10-31 14:50:40 -0400

    starter code

---
+++ src/components/AddUser.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import Card from './Card';
+import Button from './Button';
+import './AddUser.css';
+
+const AddUser = () => {
+
+  return (
+    <Card className="input">
+      <form>
+        <label>Username</label>
+        <input
+          id="username"
+          type="text"
+        />
+        <label>Age (Years)</label>
+        <input
+          id="age"
+          type="number"
+        />
+        <label>Link to image</label>
+         <input
+          id="img"
+          type="text"
+        />
+        <label>Major</label>
+         <input
+          id="major"
+          type="text"
+        />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
